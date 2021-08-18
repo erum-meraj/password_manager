efef8703016d513db1231a41da77345e55182bbe
erum-meraj <69468686+erum-meraj@users.noreply.github.com>
2021-08-19 02:18:18 +0530

    sign up page created and db connectivity done
    pls add another column to ur db called email

--- new_user.py
+++ new_user.py
@@ -1,17 +1,14 @@
-def user_record_check(name, passw, cur):
+def user_record_entry(name , passw, mail, cur, con):
     name_entered = name
     pw_entered = passw
+    email_entered = mail
     #query = "SELECT username, password, COUNT(*) FROM users WHERE username = {} and password = {}".format(name_entered, pw_entered)
-    query = "select username, password from users where username = '{}' and password = '{}'".format(name_entered, pw_entered)
-    cur.execute(query)
-    data = cur.fetchall()
-    lines = 0
-    for l in data:
-        lines = lines + 1
-    print(lines)
-    if lines == 0:
-        print ("It Does Not Exist")   
-    else:
-        print("Exists")
+    query = "INSERT INTO users( username, password, email ) VALUES('{}', '{}', '{}')".format(name_entered, pw_entered, email_entered)
+    try:
+        cur.execute(query)
+        con.commit()
+        print("registration successful ")
+    except :
+        print("registration failed ")
     #con.close();
     return
--- signup.py
+++ signup.py
@@ -2,7 +2,7 @@ import tkinter as tk
 #import requests # Download this library using pip install request!!!!!!!
 from tkinter import font
 import mysql.connector as sql
-from reg_user import user_record_check
+from new_user import user_record_entry
 from PIL import Image, ImageTk # Download this library using pip install Pillow!!!!!
 
 
@@ -16,11 +16,6 @@ root.minsize(height=982, width=700)
 con = sql.connect(host = 'localhost', user = 'root', password = 'erummeraj', database = 'pw-manager')
 cur = con.cursor(buffered=True)
 
-#sign_up_redirect_function
-def sign_up_redirect():
-    print("hello")
-
-
 #layout
 frame = tk.Frame(root, bg="#FADCD9", bd=5)
 frame.place(x=115 , y = 170 , height = 630 , width =450)
@@ -30,28 +25,31 @@ background_label = tk.Label(root, image=background_image, bg = "#FADCD9")
 background_label.place(x=140, y=180, width=400, height=200)
 
 
-welcomeback=tk.Label(frame,text = "WELCOME BACK!", font=("times new roman" , 35 , "bold") , fg = "black", bg = "#FADCD9")
-welcomeback.place(x=70 , y =260)
+welcomeback=tk.Label(frame,text = "SIGN UP!", font=("times new roman" , 35 , "bold") , fg = "black", bg = "#FADCD9")
+welcomeback.place(x=150 , y =250)
 
 username=tk.Label(frame,text = "Username", font=("times new roman" , 24 , "bold") , fg = "black", bg = "#FADCD9")
-username.place(x= 80  , y =330)
+username.place(x= 80  , y =320)
 
 entry_username = tk.Entry(frame, font=('Bookman Old Style', 18), bg="white", fg="black")
-entry_username.place(x=80  , y =360 , width = 270)
+entry_username.place(x=80  , y =350 , width = 270)
 
 password=tk.Label(frame,text = "Password", font=("times new roman" , 24 , "bold") , fg = "black", bg = "#FADCD9")
-password.place(x= 80  , y =420)
+password.place(x= 80  , y =500)
 
 bullet = "\u2022" 
 entry_password = tk.Entry(frame, font=('Bookman Old Style', 18), bg="white", fg="black", show=bullet)
-entry_password.place(x= 80  , y =450 , width = 270)
+entry_password.place(x= 80  , y =530, width = 270)
 
-button_login = tk.Button(frame, text="Login", font=('Bookman Old Style', 14), bg='#ffffff', fg="black" , command=lambda: user_record_check(entry_username.get(), entry_password.get(), cur, con))
-button_login.place(x=155,y=510, height=35, width=120)
+email=tk.Label(frame,text = "Email", font=("times new roman" , 24 , "bold") , fg = "black", bg = "#FADCD9")
+email.place(x= 80  , y =410)
 
-link1 = tk.Label(frame, text="Don't have an account?? Sign Up", fg="#787A91", font=('Bookman Old Style', 14,'underline'), cursor="hand2", bg= "#FADCD9")
-link1.place(x=120,y=560)
-link1.bind("<Button-1>", lambda e: sign_up_redirect())
+bullet = "\u2022" 
+entry_email = tk.Entry(frame, font=('Bookman Old Style', 18), bg="white", fg="black")
+entry_email.place(x= 80  , y =440 , width = 270)
+
+button_login = tk.Button(frame, text="Sign Up", font=('Bookman Old Style', 14), bg='#ffffff', fg="black" , command=lambda: user_record_entry(entry_username.get(), entry_password.get(), entry_email.get(), cur, con))
+button_login.place(x=155,y=580, height=35, width=120)
 
 
 #sign_up_redirect
